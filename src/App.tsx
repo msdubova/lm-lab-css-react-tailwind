e7f5ca0f54c576301016ebeee0b7bed36cd59b4a
Your GitHub Usernamemsdubova <femme777@gmail.com>
2023-09-06 13:04:31 +0100

    Installed tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import { PropertiesDisplay } from './components/properties-display'
 function App() {
   return (
     <>
-      <h1>Welcome to Marvelous Mansions</h1>
-      <h2>A one stop shop to your dream property!</h2>
+      <h1 className=' font-mono '>Welcome to Marvelous Mansions</h1>
+      <h2 className='font-mono'>A one stop shop to your dream property!</h2>
       <h3>Current property listings:</h3>
       <PropertiesDisplay />
     </>
--- src/components/properties-display.tsx
+++ src/components/properties-display.tsx
@@ -6,7 +6,7 @@ export const PropertiesDisplay = () => {
     console.log(propertyData.length)
     return (
         <>
-            <ul>
+            <ul className="flex flex-col">
                 {propertyData.map((p) => <Card property={p} />)}
             </ul>
         </>
